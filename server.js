d59a94ec11d0cbea567514329425e006c2d0cfa0
Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>
2020-08-02 22:47:51 +0300

    chore: implement basic authentication with no db

--- server.js
+++ server.js
@@ -5,13 +5,48 @@ const mongoose = require('mongoose');
 const { v4: uuid } = require('uuid');
 const session = require('express-session');
 const SessionStore = require('session-file-store')(session);
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const users = [
+  { id: '2f24vvg', email: 'test@test.com', password: 'password' },
+];
+
+// configure passport.js to use the local strategy
+passport.use(new LocalStrategy(
+  { usernameField: 'email' },
+  (email, password, done) => {
+    console.log('Inside local strategy callback')
+    // here is where you make a call to the database
+    // to find the user based on their username or email address
+    // for now, we'll just pretend we found that it was users[0]
+    const user = users[0] 
+    if(email === user.email && password === user.password) {
+      console.log('Local strategy returned true')
+      return done(null, user)
+    }
+  }
+));
+
+// tell passport how to serialize the user
+passport.serializeUser((user, done) => {
+  console.log('Inside serializeUser callback. User id is save to the session file store here')
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  console.log('Inside deserializeUser callback');
+  console.log(`The user id passport saved in the session file store is: ${id}`);
+  const user = users[0].id === id ? users[0] : false; 
+  done(null, user);
+});
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(session({
@@ -25,6 +60,8 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
@@ -38,6 +75,56 @@ app.get('/', (req, res) => {
   console.log(req.sessionID);
   res.send(`magical songs of enchantment: ${req.sessionID}\n`);
 });
+
+/* login routes */
+
+app.get('/login', (req, res) => {
+  console.log('Inside GET /login callback function');
+  console.log(req.sessionID);
+  res.send('i guess it should be the login page!\n');
+});
+
+app.post('/login', (req, res, next) => {
+  console.log('Inside POST /login callback function');
+  passport.authenticate('local', (err, user, info) => {
+    console.log('Inside passport.authenticate() callback');
+    console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`);
+    console.log(`req.user: ${JSON.stringify(req.user)}`);
+    req.login(user, (err) => {
+      console.log('Inside req.login() callback');
+      console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`);
+      console.log(`req.user: ${JSON.stringify(req.user)}`);
+      return res.send('You are authenticated & logged in!\n');
+    });
+  })(req, res, next);
+});
+
+app.post('/login', (req, res, next) => {
+  console.log('Inside POST /login callback')
+  passport.authenticate('local', (err, user, info) => {
+    console.log('Inside passport.authenticate() callback');
+    console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+    console.log(`req.user: ${JSON.stringify(req.user)}`)
+    req.login(user, (err) => {
+      console.log('Inside req.login() callback')
+      console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+      console.log(`req.user: ${JSON.stringify(req.user)}`)
+      return res.send('You were authenticated & logged in!\n');
+    })
+  })(req, res, next);
+});
+
+app.get('/authrequired', (req, res) => {
+  console.log('Inside GET /authrequired callback');
+  console.log(`User authenticated? ${req.isAuthenticated()}`);
+  if(req.isAuthenticated()) {
+    res.send('you hit the authentication endpoint\n');
+  } else {
+    res.redirect('/');
+  }
+});
+
+/* start listening port */
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
