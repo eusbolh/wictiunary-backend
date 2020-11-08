c99b6a0af899dee392d9b460c4294561739869ba
Cüneyt Emre Yavuz <cuneytemre.yavuz@commencis.com>
2020-11-08 23:20:31 +0300

    chore: update some responses and code style

--- server.js
+++ server.js
@@ -1,68 +1,79 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const { v4: uuid } = require('uuid');
-const session = require('express-session');
-const SessionStore = require('session-file-store')(session);
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt-nodejs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { v4: uuid } = require("uuid");
 
-require('dotenv').config();
+// Session imports
+const session = require("express-session");
+const SessionStore = require("session-file-store")(session);
 
-/* Establish MongoDB connection */
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+// Passport imports
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+// Encryption imports
+const bcrypt = require("bcrypt-nodejs");
+
+// Get dotenv
+require("dotenv").config();
+
+// Establish MongoDB connection
+mongoose.connect(process.env.ATLAS_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully.');
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully.");
 });
 
-/* Get db models */
-const User = require('./models/user.model');
+// Get db models
+const User = require("./models/user.model");
 
 // configure passport.js to use the local strategy
-passport.use(new LocalStrategy(
-  { usernameField: 'email' },
-  (email, password, done) => {
-    console.log('Inside local strategy callback');
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+    console.log("Inside local strategy callback");
 
     // Check if credentials are valid
-    User
-      .find({ email })
+    User.find({ email })
       .then((response) => {
         const user = response && response[0];
         if (!bcrypt.compareSync(password, user && user.password)) {
-          return done(null, false, { message: 'Invalid credentials.\n' });
+          return done(null, false, { message: "Invalid credentials.\n" });
         }
         return done(null, user);
       })
       .catch((error) => {
         done(error);
       });
-  }
-));
+  })
+);
 
 // tell passport how to serialize the user
 passport.serializeUser((user, done) => {
-  console.log('Inside serializeUser callback. User id is save to the session file store here')
+  console.log(
+    "Inside serializeUser callback. User id is save to the session file store here"
+  );
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  console.log('Inside deserializeUser callback');
+  console.log("Inside deserializeUser callback");
   console.log(`The user id passport saved in the session file store is: ${id}`);
-  
+
+  console.log(id);
+
   // Fetch user data from db
-  User
-    .find({ id })
+  User.find({ _id: id })
     .then((response) => {
       const user = response && response[0];
       if (user) {
         done(null, user);
       } else {
-        done('User not found', false);
+        done("User not found", false);
       }
     })
     .catch((error) => {
@@ -76,39 +87,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(session({
-  genid: (req) => {
-    console.log('Inside the session middleaware');
-    console.log(req.sessionID);
-    return uuid();
-  },
-  store: new SessionStore(),
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-}));
+app.use(
+  session({
+    genid: (req) => {
+      console.log("Inside the session middleaware");
+      console.log(req.sessionID);
+      return uuid();
+    },
+    store: new SessionStore(),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req, res) => {
-  console.log('Inside the homepage callback function');
-  console.log(req.sessionID);
-  res.send(`magical songs of enchantment: ${req.sessionID}\n`);
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to Wictiunary!");
 });
 
 /* login routes */
 
-app.get('/login', (req, res) => {
-  console.log('Inside GET /login callback function');
-  console.log(req.sessionID);
-  res.send('i guess it should be the login page!\n');
-});
-
-app.post('/register', (req, res, next) => {
+app.post("/register", (req, res, next) => {
   const { email, password } = req.body;
 
   let encryptedPassword;
-  bcrypt.genSalt(10, function(err, salt) {
+  bcrypt.genSalt(10, function (err, salt) {
     bcrypt.hash(password, salt, null, (err, hash) => {
       if (!err) {
         encryptedPassword = hash;
@@ -129,28 +134,18 @@ app.post('/register', (req, res, next) => {
       }
     });
   });
+});
 
-  /*
-  
-  // todo: check arguments
-
-  user
-    .save()
-    .then(() => {
-      res.json('User registered!');
-    })
-    .catch((err) => {
-      res.status(400).json('Error: ', err);
-    });
-    */
-})
-
-app.post('/login', (req, res, next) => {
-  console.log('Inside POST /login callback function');
-  passport.authenticate('local', (error, user, info) => {
-    console.log('Inside passport.authenticate() callback');
-    console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`);
+app.post("/login", (req, res, next) => {
+  console.log("Inside POST /login callback function");
+  passport.authenticate("local", (error, user, info) => {
+    console.log("Inside passport.authenticate() callback");
+    console.log(
+      `req.session.passport: ${JSON.stringify(req.session.passport)}`
+    );
     console.log(`req.user: ${JSON.stringify(req.user)}`);
+
+    console.log(error, user, info);
 
     if (info) {
       return res.send(info && info.message); // TODO: return proper http response
@@ -159,7 +154,7 @@ app.post('/login', (req, res, next) => {
       return next(error);
     }
     if (!user) {
-      return res.redirect('/login'); // TODO: return proper http response
+      return res.redirect("/login"); // TODO: return proper http response
     }
 
     req.login(user, (error) => {
@@ -171,13 +166,13 @@ app.post('/login', (req, res, next) => {
   })(req, res, next);
 });
 
-app.get('/authrequired', (req, res) => {
-  console.log('Inside GET /authrequired callback');
+app.get("/authrequired", (req, res) => {
+  console.log("Inside GET /authrequired callback");
   console.log(`User authenticated? ${req.isAuthenticated()}`);
-  if(req.isAuthenticated()) {
-    res.send('you hit the authentication endpoint\n');
+  if (req.isAuthenticated()) {
+    res.send("you hit the authentication endpoint\n");
   } else {
-    res.redirect('/');
+    res.status(401).send();
   }
 });
 
