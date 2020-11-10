9d33d5c1b9cfbfae62d0e7eb927e3833ae74d9fe
Cüneyt Emre Yavuz <cuneytemre.yavuz@commencis.com>
2020-11-10 21:41:28 +0300

    feat: add word model and implement db layer to word search

--- routes/dict.route.js
+++ routes/dict.route.js
@@ -1,11 +1,28 @@
 const router = require("express").Router();
 const fetch = require("node-fetch");
+const Word = require("../models/word.model");
 
 const BASE_URL = "https://wordsapiv1.p.rapidapi.com/";
 
 router.route("/search").post(async function (req, res, next) {
   const { word } = req.body;
   // TODO: add null check to word
+
+  const wordsInDB = await Word.find({ word })
+    .then((response) => {
+      console.log(response);
+      return response;
+    })
+    .catch((err) => {
+      // TODO: log error
+    });
+
+  if (wordsInDB && wordsInDB.length > 0) {
+    // TODO: should i clean ids from objects??
+    console.log("Word is found in DB");
+    res.status(200).send(wordsInDB[0]);
+    return;
+  }
 
   const data = await fetch(`${BASE_URL}/words/${word}/definitions`, {
     method: "GET",
@@ -18,6 +35,20 @@ router.route("/search").post(async function (req, res, next) {
     })
     .catch((err) => {
       // TODO: should i throw error here?
+    });
+
+  const wordToSave = new Word({
+    word: data.word,
+    definitions: data.definitions,
+  });
+
+  await wordToSave
+    .save()
+    .then(() => {
+      // TODO: log success
+    })
+    .catch((err) => {
+      // TODO: log error
     });
 
   res.status(200).send(data);
